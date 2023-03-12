ccb78b9e5a86187b9d48cdf5716d947465fa03d8
Raphael Beer <raphael.beer@ict-campus.net>
2023-03-12 16:09:06 +0100

    Edit Config

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECURITY_KEY_FORMS'
-    SERVER_NAME='localhost:5000'
+    SERVER_NAME='wa-todo-prod-02aaf488.azurewebsites.net:443'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
